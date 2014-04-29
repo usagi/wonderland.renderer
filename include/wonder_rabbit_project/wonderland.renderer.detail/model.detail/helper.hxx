7e88f9fd12b8d0853dab90a00bde4ffc039e2cc6
Usagi Ito <usagi@WonderRabbitProject.net>
2014-04-30 00:17:28 +0900

    add model helper

---
+++ include/wonder_rabbit_project/wonderland.renderer.detail/model.detail/helper.hxx
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec2.hpp>
+
+#include <assimp/matrix4x4.h>
+#include <assimp/vector3.h>
+
+namespace wonder_rabbit_project
+{
+  namespace wonderland
+  {
+    namespace renderer
+    {
+      namespace model
+      {
+        namespace helper
+        {
+
+          // Assimp::aiMatrix4x4t
+          //  http://assimp.sourceforge.net/lib_html/classai_matrix4x4t.html
+          // glm::mat4
+          //  https://github.com/g-truc/glm/blob/0.9.5/glm/detail/type_mat4x4.hpp
+          template < class T = void >
+          inline auto to_glm_mat4( const aiMatrix4x4& in )
+            -> glm::mat4
+          {
+            return
+              { in[0][0], in[0][1], in[0][2], in[0][3]
+              , in[1][0], in[1][1], in[1][2], in[1][3]
+              , in[2][0], in[2][1], in[2][2], in[2][3]
+              , in[3][0], in[3][1], in[3][2], in[3][3]
+              };
+          }
+          
+          template < class T = void >
+          inline auto to_glm_vec4( const aiVector3D* in ) -> glm::vec4
+          { return { in -> x, in -> y, in -> z, 1.0f }; }
+
+          template < class T = void >
+          inline auto to_glm_vec3( const aiVector3D* in ) -> glm::vec3
+          { return { in -> x, in -> y, in -> z }; }
+          
+          template < class T = void >
+          inline auto to_glm_vec2( const aiVector3D* in ) -> glm::vec2
+          { return { in -> x, in -> y }; }
+          
+        }
+      }
+    }
+  }
+}
