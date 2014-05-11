fc72f90bbe4179b3580ae82fed3b236c6481610c
Usagi Ito <usagi@WonderRabbitProject.net>
2014-05-12 01:44:17 +0900

    update to the latest example

--- example/example-1/main.cxx
+++ example/example-1/main.cxx
@@ -6,129 +6,156 @@
 #include <array>
 
 #ifdef EMSCRIPTEN
-  #include <emscripten/emscripten.h>
+#include <emscripten/emscripten.h>
 #endif
 
 #include <wonder_rabbit_project/wonderland.renderer.hxx>
 #include <wonder_rabbit_project/wonderland.subsystem.hxx>
 
-namespace
-{
-  auto vs_source() -> std::string
-  { return 
-      "#version 100\n"
-      "attribute vec4 position;\n"
-      "void main(void)\n"
-      "{\n"
-      "  gl_Position = position;\n"
-      "}"
-      ;
-  }
-  
-  auto fs_source() -> std::string
-  { return
-      "#version 100\n"
-      "void main(void)\n"
-      "{\n"
-      "  gl_FragColor = vec4( 1., 0., 0., 1. );\n"
-      "}"
-      ;
-  }
-  
-  /*
-  auto build_vao(const std::vector<std::array<float, 2>>& vertices)
-    -> GLuint
-  {
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 2 * vertices.size(), vertices.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(0);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    
-    return vao;
-  }
-  */
-}
-
 auto main()
-  -> int
-  try
+-> int
+try
 {
   using namespace wonder_rabbit_project::wonderland::subsystem;
-  using namespace wonder_rabbit_project::wonderland::renderer::glew;
+  using namespace wonder_rabbit_project::wonderland::renderer;
+  using wonder_rabbit_project::wonderland::renderer::model::animation_state_t;
+  
+  constexpr auto screen_width  = 1920 >> 1;
+  constexpr auto screen_height = 1080 >> 1;
   
   auto subsystem = std::make_shared<subsystem_t>();
   
   auto ips = subsystem -> default_initialize_params();
-  ips.put( "title", "wonderland.subsystem example-2" );
+  ips.put( "width"  , screen_width  );
+  ips.put( "height" , screen_height );
+  ips.put( "title"  , "assimp on g++/clang++/em++ test" );
+  ips.put( "samples", 4 ); // FSAA
   subsystem -> initialize( std::move(ips) );
   
-  auto renderer = glew::instance();
-  renderer -> initialize();
+  auto renderer = std::make_shared<renderer_t>();
   
-  auto vs = renderer -> create_shader<vertex_shader  >( vs_source() );
-  auto fs = renderer -> create_shader<fragment_shader>( fs_source() );
-  auto program = renderer -> create_program( vs, fs );
+  // wonderland.renderer has the standard shader program
+  auto program = renderer -> create_program_standard();
   
-  using va_pos2f_tex2f = vertex
-  < std::array<float, 2> // position
-  , std::array<float, 2> // texcoord
-  >;
+  auto model = renderer -> create_model( "assets/dwarf/dwarf1.x" );
   
-  auto model = renderer -> create_model< va_pos2f_tex2f, usage_static_draw, mode_line_loop >
-  ( { { {{ -.5f, -.5f }}, {{0, 0}} }
-    , { {{  .5f, -.5f }}, {{1, 0}} }
-    , { {{  .5f,  .5f }}, {{1, 1}} }
-    , { {{ -.5f,  .5f }}, {{0, 1}} }
-    }
-  );
+  struct model_instance_state_t
+  {
+    animation_state_t animation_state;
+    glm::mat4         world_transformation;
+  };
   
-  renderer -> clear_color( { 0.f, 0.f, 1.f, 0.f } );
+  std::array< model_instance_state_t, 9 > model_instance_states;
   
-  subsystem -> update_functors.emplace_front
-  ( []
+  {
+    auto animation_names = model.animation_names();
+    
+    if ( animation_names.size() )
     {
+      for ( const auto& animation_name : animation_names )
+        std::cout << "has animation: " << animation_name << std::endl;
       
+      for ( auto n = 0; n < model_instance_states.size(); ++n )
+      {
+        auto& s = model_instance_states[ n ];
+        
+        s.animation_state.name = animation_names[0];
+        s.animation_state.time_in_seconds = float( n * 19937 );
+        
+        s.world_transformation = glm::scale( glm::mat4(), glm::vec3( 13.0f ) );
+        switch ( n )
+        { case 1: s.world_transformation = glm::translate( s.world_transformation, glm::vec3(  10.0f, 0.0f,   0.0f ) ); break;
+          case 2: s.world_transformation = glm::translate( s.world_transformation, glm::vec3( -10.0f, 0.0f,   0.0f ) ); break;
+          case 3: s.world_transformation = glm::translate( s.world_transformation, glm::vec3(  10.0f, 0.0f,  10.0f ) ); break;
+          case 4: s.world_transformation = glm::translate( s.world_transformation, glm::vec3(   0.0f, 0.0f,  10.0f ) ); break;
+          case 5: s.world_transformation = glm::translate( s.world_transformation, glm::vec3( -10.0f, 0.0f,  10.0f ) ); break;
+          case 6: s.world_transformation = glm::translate( s.world_transformation, glm::vec3(  10.0f, 0.0f, -10.0f ) ); break;
+          case 7: s.world_transformation = glm::translate( s.world_transformation, glm::vec3(   0.0f, 0.0f, -10.0f ) ); break;
+          case 8: s.world_transformation = glm::translate( s.world_transformation, glm::vec3( -10.0f, 0.0f, -10.0f ) );
+        }
+      }
     }
+  }
+  
+  // clear color
+  {
+    const auto color = glm::vec4( 0.8f, 0.8f, 1.0f, 1.0f );
+    renderer -> clear_color( color );
+  }
+  
+  // projection transformation
+  {
+    const auto fov_y = glm::pi<float>() / 4.0f;
+    constexpr auto screen_aspect_ratio = float( screen_width ) / float( screen_height );
+    constexpr auto near_clip =    0.1f;
+    constexpr auto far_clip  = 1000.0f;
+    renderer -> projection_transformation( glm::perspective( fov_y, screen_aspect_ratio, near_clip, far_clip ) );
+  }
+  
+  subsystem -> update_functors.emplace_front
+  ( [ subsystem, renderer, &model_instance_states ]
+  {
+    static auto counter = 0.0f;
+    
+    static auto distance = 100.0f;
+    
+    for ( auto& model_instance_state : model_instance_states )
+      model_instance_state.animation_state += std::chrono::milliseconds( 16 );
+    
+    const auto wheel = subsystem -> pointing_states_wheel();
+    
+    if ( wheel.y > 0 )
+      distance -= 10.0f;
+    else if ( wheel.y < 0 )
+      distance += 10.0f;
+    
+    // camera as view transformation generator
+    auto& camera = renderer -> camera();
+    camera.eye   ( { distance * std::sin( counter ), 100.0f, distance * std::cos( counter ) } );
+    camera.target( { 0.0f, 25.0f, 0.0f } );
+    camera.up    ( { 0.0f,  1.0f, 0.0f } );
+    
+    counter += 0.01f;
+  }
   );
   
   subsystem -> update_functors.emplace_front
-    ( [ subsystem ]
-      {
-        if ( subsystem -> keyboard_state< key::escape >() )
-          subsystem -> to_continue( false );
-      }
-    );
+  ( [ subsystem ]
+  {
+    if ( subsystem -> keyboard_state< key::escape >() )
+      subsystem -> to_continue( false );
+  }
+  );
   
   subsystem -> render_functors.emplace_front
-  ( [ renderer, &program, &model /*&vertices, &vao*/ ]
-    {
-      renderer -> clear();
-      auto flusher     = renderer -> flusher();
-      auto use_program = renderer -> use_program( program );
-      //glBindVertexArray( vao );
-      //glDrawArrays( GL_LINE_LOOP, 0, vertices.size() );
-      //glBindVertexArray( 0 );
-    }
+  ( [ renderer
+  , &program, &model
+  , &model_instance_states
+  ]
+  {
+    renderer -> clear();
+    auto flusher     = renderer -> flusher();
+    auto use_program = renderer -> use_program( program );
+    
+    // draw model with world transformation
+    //  automatic set
+    //   1. uniform(world_view_projection_transformation) if shader program has uniform world_view_projection_transformation
+    //   2. uniform(world_transformation) if shader program has uniform world_transformation
+    
+    for ( const auto& model_instance_state : model_instance_states )
+      renderer -> draw( model, model_instance_state.world_transformation, { model_instance_state.animation_state } );
+  }
   );
   
   subsystem -> render_functors.emplace_front
   ( [ ]
-    {
-      static auto before = std::chrono::high_resolution_clock::now();
-      const auto elapsed = std::chrono::high_resolution_clock::now() - before;
-      const auto wait = std::chrono::milliseconds(33) - elapsed;
-      std::this_thread::sleep_for(wait);
-      before = std::chrono::high_resolution_clock::now();
-    }
+  {
+    static auto before = std::chrono::high_resolution_clock::now();
+    const auto elapsed = std::chrono::high_resolution_clock::now() - before;
+    const auto wait = std::chrono::milliseconds(33) - elapsed;
+    std::this_thread::sleep_for(wait);
+    before = std::chrono::high_resolution_clock::now();
+  }
   );
   
   subsystem -> invoke();
